6cf800f8a6c3f88940e75aee13ce1991e8c684aa
eltx1 <info.eltx@gmail.com>
2026-08-15 05:15:42 +0300

    build: keep applicant shell on stable Vite entries

--- resources/views/layouts/applicant.blade.php
+++ resources/views/layouts/applicant.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Publisher Application') · Horus Media</title>
     <x-brand.favicons />
-    @vite(['resources/css/app.css', 'resources/css/publisher-application.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="applicant-shell">
