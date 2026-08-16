cc36e87960e84b58708ac1d2075cac70c0842d7a
eltx1 <info.eltx@gmail.com>
2026-08-16 15:53:54 +0300

    Cast site management role explicitly

--- app/Models/SiteServingSetting.php
+++ app/Models/SiteServingSetting.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\PrebidConfiguredMode;
 use App\Enums\ServingMode;
+use App\Enums\SiteManagementRole;
 use App\Models\Concerns\BelongsToOrganization;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
@@ -13,7 +14,7 @@ class SiteServingSetting extends Model
 {
     use BelongsToOrganization, HasUlids;
 
-    protected $fillable = ['organization_id', 'site_id', 'serving_mode', 'revenue_share_percent', 'prebid_enabled', 'prebid_configured_mode', 'native_demand_enabled', 'monetization_manager_domain', 'monetization_manager_relationship', 'monetization_manager_country', 'placement_plan', 'configuration_version'];
+    protected $fillable = ['organization_id', 'site_id', 'serving_mode', 'revenue_share_percent', 'prebid_enabled', 'prebid_configured_mode', 'native_demand_enabled', 'monetization_manager_role', 'monetization_manager_domain', 'monetization_manager_relationship', 'monetization_manager_country', 'placement_plan', 'configuration_version'];
 
     protected function casts(): array
     {
@@ -23,6 +24,7 @@ class SiteServingSetting extends Model
             'prebid_enabled' => 'boolean',
             'prebid_configured_mode' => PrebidConfiguredMode::class,
             'native_demand_enabled' => 'boolean',
+            'monetization_manager_role' => SiteManagementRole::class,
             'placement_plan' => 'array',
         ];
     }
