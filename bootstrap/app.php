27b7b8cc0ec3901bd8465d898f42cbc8530f95fd
eltx1 <info.eltx@gmail.com>
2026-08-17 17:00:48 +0300

    Task 46: load account center routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function (): void {
+            Route::middleware('web')->group(base_path('routes/account.php'));
             Route::middleware('web')->group(base_path('routes/admin-auth.php'));
             Route::middleware('web')->group(base_path('routes/prebid-ads-txt.php'));
             Route::middleware('web')->group(base_path('routes/platform-ads-txt.php'));
