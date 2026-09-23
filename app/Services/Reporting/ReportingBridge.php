7d00cda74faa4062b9e9318c5811cc4d6160eadc
eltx1 <info.eltx@gmail.com>
2026-09-24 00:06:07 +0300

    Force all GAM reporting bridges to canonical USD

--- app/Services/Reporting/ReportingBridge.php
+++ app/Services/Reporting/ReportingBridge.php
@@ -37,7 +37,7 @@ final class ReportingBridge
             $gam->id,
             $gam->name,
             $gam->network_code,
-            data_get($gam->configuration, 'currency', config('reporting.default_currency', 'USD')),
+            config('reporting.canonical_currency', 'USD'),
             $actor,
         );
     }
