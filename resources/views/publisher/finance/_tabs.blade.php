ebf9eadbbead3cdca018997d0ccaf31ae1afe838
eltx1 <info.eltx@gmail.com>
2026-09-23 05:58:29 +0300

    Clarify Publisher finance tabs

--- resources/views/publisher/finance/_tabs.blade.php
+++ resources/views/publisher/finance/_tabs.blade.php
@@ -1,5 +1,5 @@
 <x-control-plane.workspace-tabs :items="[
-    ['label' => 'Earnings', 'href' => route('publisher.finance.overview')],
+    ['label' => 'Reports & earnings', 'href' => route('publisher.finance.overview')],
     ['label' => 'Statements', 'href' => route('publisher.finance.statements.index')],
     ['label' => 'Payment method', 'href' => route('publisher.finance.payment-method.edit')],
     ['label' => 'Payouts', 'href' => route('publisher.finance.payouts.index')],
