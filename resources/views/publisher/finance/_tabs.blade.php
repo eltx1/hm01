ad955c32ca2f2b6a67639aebd3f9d82583728edb
eltx1 <info.eltx@gmail.com>
2026-09-23 21:14:25 +0300

    Port UX rebuild: resources/views/publisher/finance/_tabs.blade.php

--- resources/views/publisher/finance/_tabs.blade.php
+++ resources/views/publisher/finance/_tabs.blade.php
@@ -1,6 +1,6 @@
 <x-control-plane.workspace-tabs :items="[
-    ['label' => 'Overview', 'href' => route('publisher.finance.overview')],
+    ['label' => 'Reports & earnings', 'href' => route('publisher.finance.overview')],
     ['label' => 'Statements', 'href' => route('publisher.finance.statements.index')],
-    ['label' => 'Payment Method', 'href' => route('publisher.finance.payment-method.edit')],
-    ['label' => 'Payout History', 'href' => route('publisher.finance.payouts.index')],
-]" label="Earnings and Payments sections" />
+    ['label' => 'Payment details', 'href' => route('publisher.finance.payment-method.edit')],
+    ['label' => 'Payout history', 'href' => route('publisher.finance.payouts.index')],
+]" label="Reports and earnings sections" />
