13f05679dfabba5d273075839ff0a3e18a8e0866
eltx1 <info.eltx@gmail.com>
2026-09-23 03:35:03 +0300

    Simplify Publisher finance navigation labels

--- resources/views/publisher/finance/_tabs.blade.php
+++ resources/views/publisher/finance/_tabs.blade.php
@@ -1,6 +1,6 @@
 <x-control-plane.workspace-tabs :items="[
-    ['label' => 'Overview', 'href' => route('publisher.finance.overview')],
-    ['label' => 'Statements', 'href' => route('publisher.finance.statements.index')],
-    ['label' => 'Payment Method', 'href' => route('publisher.finance.payment-method.edit')],
-    ['label' => 'Payout History', 'href' => route('publisher.finance.payouts.index')],
+    ['label' => 'Earnings overview', 'href' => route('publisher.finance.overview')],
+    ['label' => 'Monthly statements', 'href' => route('publisher.finance.statements.index')],
+    ['label' => 'Payment details', 'href' => route('publisher.finance.payment-method.edit')],
+    ['label' => 'Payouts', 'href' => route('publisher.finance.payouts.index')],
 ]" label="Earnings and Payments sections" />
